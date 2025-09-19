06d7d6114ca1d1f9c7b462cb8942ffed179aaaa1
legendofmaj <legendofmaj.unaired760@passmail.com>
2025-09-19 23:25:58 +0200

    feat: show watched episodes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 // this is the same thing as `using namespace` in C++
 use serde_json::json;
 use reqwest::Client;
-// use text_io::read;
-
-// fn print() 
-// {
-//   println!("Type something...");
-//   let word: String = read!();
-//   println!("You typed: {}", word);
-// }
 
 fn main()
 {
@@ -17,18 +9,19 @@ fn main()
 
   // parse json
   let minutes = result["data"]["User"]["statistics"]["anime"]["minutesWatched"].as_i64().unwrap(); //or as_f64 if I wanted a float.
+  let episodes = result["data"]["User"]["statistics"]["anime"]["episodesWatched"].as_i64().unwrap(); //or as_f64 if I wanted a float.
   let username = result["data"]["User"]["name"].to_string();
-  let username = username.replace('"', "");
+  let username = username.replace('"', ""); //remove " from string
 
-  // perform calculations
+  // perform calculation
   let hours = minutes / 60;
 
   // print to screen
-  println!("{} watched {} hours of TV ({} minutes)", username, hours, minutes);
+  println!("{} watched {} episodes making for a total  of {} hours ({} minutes).", username, episodes, hours, minutes);
 }
 
 const QUERY: &str = "
-query ($name: String) { # Define which variables will be used in the query (id)
+query ($name: String) {
   User(name: $name)
   {
     name
@@ -61,14 +54,4 @@ async fn request() -> serde_json::Value {
     let result: serde_json::Value = serde_json::from_str(&resp.unwrap()).unwrap();
     
     return result;
-
-    // parse json
-    //println!("{}", result);
-    //println!("Minutes watched: {}", result["data"]["User"]["statistics"]["anime"]["minutesWatched"]);
-    //let minutes = result["data"]["User"]["statistics"]["anime"]["minutesWatched"].clone();
-   // println!("{}", minutes);
-    
-
-    //println!("{:#}", result);
-    //println!("{}", minutes);
 }
